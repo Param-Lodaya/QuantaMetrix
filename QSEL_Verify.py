f699fcf83e871ff01788819a36ca988d4e9d8c48
Param-Lodaya <paramlodaya.225062@gmail.com>
2025-08-28 14:05:46 +0530

    Delete QSEL_Verify.py

--- QSEL_Verify.py
+++
@@ -1,9 +0,0 @@
-from QSEL_Generation import qsel_state, state_token
-
-def qsel_to_state(qsel_str):
- parts = qsel_str.split('|')
- if len(parts) !=8:
-  raise ValueError(f"Invalid: 8 Parts Needed, got {len(parts)}")
- counts = [int(p[3:]) for p in parts]
- total = sum(counts)
- return [c/total for c in counts] if total else [0]*8
