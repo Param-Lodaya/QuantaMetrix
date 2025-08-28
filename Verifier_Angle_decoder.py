c228c3453959019e1651092db41c5ae6e5df7a9a
Param-Lodaya <paramlodaya.225062@gmail.com>
2025-08-28 14:05:02 +0530

    Delete Verifier_Angle_decoder.py

--- Verifier_Angle_decoder.py
+++
@@ -1,9 +0,0 @@
-def decode_timestamp(ts):
- try:
-    _, data= ts.strip().split()
- except ValueError: 
-    raise ValueError("Invalid Input Detected!")
- parts = data.split('-')
- if len(parts) !=9:
-    raise ValueError(f"Invalid: 9 Parts Needed!, got{len(parts)}")
- return [round(int(p)/100.0,2) for p in parts]
